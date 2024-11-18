8060f427c33ff439c093f40e82c3d6c456449ad6
Chris Morris <chris@morris.net>
2024-11-18 14:21:25 +0000

    Manually create a redemption output for debugging (commented out)

--- x/zenbtc/keeper/msg_server_submit_unsigned_redemption_tx.go
+++ x/zenbtc/keeper/msg_server_submit_unsigned_redemption_tx.go
@@ -13,6 +13,18 @@ import (
 func (k msgServer) SubmitUnsignedRedemptionTx(goCtx context.Context, msg *types.MsgSubmitUnsignedRedemptionTx) (*types.MsgSubmitUnsignedRedemptionTxResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
+	//For Debugging Only
+	//if err := k.validationKeeper.ZenBTCRedemptions.Set(ctx, 0, types.Redemption{
+	//	Data: types.RedemptionData{ // populate data below
+	//		Id:                 1,
+	//		DestinationAddress: []byte("tb1qqfgae6jr3xyawcz7uuh7ez06eteksxgf3up80x"),
+	//		Amount:             10000,
+	//	},
+	//	Completed: false,
+	//}); err != nil {
+	//	return nil, err
+	//}
+
 	// TODO: here we must check if msg.Creator is the proxy
 
 	//
@@ -26,12 +38,13 @@ func (k msgServer) SubmitUnsignedRedemptionTx(goCtx context.Context, msg *types.
 		hashes[i] = input.Hash
 	}
 
-	k.treasuryKeeper.HandleSignatureRequest(ctx, &treasurytypes.MsgNewSignatureRequest{
+	sigReq := &treasurytypes.MsgNewSignatureRequest{
 		Creator:        msg.Creator,
 		KeyIds:         keyIDs,
 		DataForSigning: strings.Join(hashes, ","), // hex string, each unsigned utxo is separated by comma
 		CacheId:        msg.CacheId,
-	})
+	}
+	k.treasuryKeeper.HandleSignatureRequest(ctx, sigReq)
 
 	return &types.MsgSubmitUnsignedRedemptionTxResponse{}, nil
 }
