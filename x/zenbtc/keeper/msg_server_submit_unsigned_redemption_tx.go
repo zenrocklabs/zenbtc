9474dde6bfd0731f0a24329bf11e47d03adc6da9
Chris Morris <chris@morris.net>
2024-11-19 12:39:50 +0000

    Temporarily Ignore verification & auto insert a pending redemption

--- x/zenbtc/keeper/msg_server_submit_unsigned_redemption_tx.go
+++ x/zenbtc/keeper/msg_server_submit_unsigned_redemption_tx.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"github.com/Zenrock-Foundation/zrchain/v5/bitcoin"
 	"github.com/btcsuite/btcd/txscript"
+	"github.com/zenrocklabs/zenrock/bitcoinproxy/libs/utils"
 	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	treasurytypes "github.com/Zenrock-Foundation/zrchain/v5/x/treasury/types"
+
 	"github.com/zenrocklabs/zenbtc/x/zenbtc/types"
 )
 
@@ -17,16 +19,16 @@ func (k msgServer) SubmitUnsignedRedemptionTx(goCtx context.Context, msg *types.
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	//For Debugging Only
-	//if err := k.validationKeeper.ZenBTCRedemptions.Set(ctx, 0, types.Redemption{
-	//	Data: types.RedemptionData{ // populate data below
-	//		Id:                 1,
-	//		DestinationAddress: []byte("tb1qqfgae6jr3xyawcz7uuh7ez06eteksxgf3up80x"),
-	//		Amount:             10000,
-	//	},
-	//	Completed: false,
-	//}); err != nil {
-	//	return nil, err
-	//}
+	if err := k.validationKeeper.ZenBTCRedemptions.Set(ctx, 0, types.Redemption{
+		Data: types.RedemptionData{ // populate data below
+			Id:                 1,
+			DestinationAddress: []byte("tb1qqfgae6jr3xyawcz7uuh7ez06eteksxgf3up80x"),
+			Amount:             10000,
+		},
+		Completed: false,
+	}); err != nil {
+		return nil, err
+	}
 
 	// TODO: here we must check if msg.Creator is the proxy
 
@@ -34,7 +36,12 @@ func (k msgServer) SubmitUnsignedRedemptionTx(goCtx context.Context, msg *types.
 	// TODO: verification against k.validationKeeper.ZenBTCRedemptions goes here
 	//
 
-	err := k.VerifyUnsignedRedemptionTX(ctx, msg)
+	//get redemptio
+
+	//err := k.VerifyUnsignedRedemptionTX(ctx, msg)
+	//if err != nil {
+	//	return nil, err
+	//	}
 
 	keyIDs := make([]uint64, len(msg.Inputs))
 	hashes := make([]string, len(msg.Inputs))
@@ -78,18 +85,50 @@ func (k msgServer) VerifyUnsignedRedemptionTX(ctx context.Context, msg *types.Ms
 	if len(msgTX.TxOut) == 0 {
 		return fmt.Errorf("BTC Transaction has zero outputs")
 	}
-
-	chain := msg.ChainName
-
+	chaincfg := utils.ChainFromString(msg.ChainName)
 	changeOutput := msgTX.TxOut[0]
+
+	//Loop all the change address and see if they match the change address in Output 0
+	_, addrs, _, err := txscript.ExtractPkScriptAddrs(changeOutput.PkScript, chaincfg)
+	if addrs == nil || len(addrs) != 1 {
+		return fmt.Errorf("BTC Change Address Invalid")
+	}
+	changeAddress := addrs[0].String()
+
+	validChangeAddress := false
 	for _, keyID := range zenBTCChangeAddressKeyIDs {
-
-		_, addrs, _, err := txscript.ExtractPkScriptAddrs(output.PkScript, chaincfg)
-
-		if changeOutput == byte(keyID) {
-			return nil
+		key, err := k.Keeper.treasuryKeeper.KeyStore.Get(ctx, keyID)
+		if err != nil {
+			return fmt.Errorf("BTC Error retrieving Change Addresses")
+		}
+		address, err := treasurytypes.BitcoinP2WPKH(&key, chaincfg)
+		if err != nil {
+			return fmt.Errorf("BTC Error Generating Change Address from Key")
+		}
+		if address == changeAddress {
+			validChangeAddress = true
+			break
 		}
 	}
-	return fmt.Errorf("BTC Transaction first output is not a change address")
+	if !validChangeAddress {
+		return fmt.Errorf("Invalid Change Address")
+	}
+
+	//Check Each Output (apart from change is in the redemption list)
+
+	//TODO: We need to check that all the Output correspond to pending Redemptions
+
+	//redemptions := k.Keeper.Redemptions()
+	//
+	//for i, output := range msgTX.TxOut[1:] {
+	//	//Dervice output address
+	//	_, addrs, _, err := txscript.ExtractPkScriptAddrs(output.PkScript, chaincfg)
+	//	if err != nil || addrs == nil || len(addrs) != 1 {
+	//		return fmt.Errorf("Redemption: Invalid Output Address %d output #", i)
+	//	}
+	//	//Check Output is in redemption list
+	//
+	//}
+
 	return nil
 }
