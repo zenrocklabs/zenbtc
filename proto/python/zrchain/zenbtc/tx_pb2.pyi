83a81806f01a056ad933ea8c359222e979faf095
Chris Morris <chris@morris.net>
2024-11-19 09:58:01 +0000

    proto built

--- proto/python/zrchain/zenbtc/tx_pb2.pyi
+++ proto/python/zrchain/zenbtc/tx_pb2.pyi
@@ -47,16 +47,18 @@ class MsgVerifyDepositBlockInclusionResponse(_message.Message):
     def __init__(self) -> None: ...
 
 class MsgSubmitUnsignedRedemptionTx(_message.Message):
-    __slots__ = ("creator", "inputs", "txbytes", "cacheId")
+    __slots__ = ("creator", "inputs", "txbytes", "cacheId", "chain_name")
     CREATOR_FIELD_NUMBER: _ClassVar[int]
     INPUTS_FIELD_NUMBER: _ClassVar[int]
     TXBYTES_FIELD_NUMBER: _ClassVar[int]
     CACHEID_FIELD_NUMBER: _ClassVar[int]
+    CHAIN_NAME_FIELD_NUMBER: _ClassVar[int]
     creator: str
     inputs: _containers.RepeatedCompositeFieldContainer[InputHashes]
     txbytes: bytes
     cacheId: bytes
-    def __init__(self, creator: _Optional[str] = ..., inputs: _Optional[_Iterable[_Union[InputHashes, _Mapping]]] = ..., txbytes: _Optional[bytes] = ..., cacheId: _Optional[bytes] = ...) -> None: ...
+    chain_name: str
+    def __init__(self, creator: _Optional[str] = ..., inputs: _Optional[_Iterable[_Union[InputHashes, _Mapping]]] = ..., txbytes: _Optional[bytes] = ..., cacheId: _Optional[bytes] = ..., chain_name: _Optional[str] = ...) -> None: ...
 
 class InputHashes(_message.Message):
     __slots__ = ("hash", "keyid")
