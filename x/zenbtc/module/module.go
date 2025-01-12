d668796df7ecd7e87e594d5b31c318d31ea8fb74
Sasha Duke <sasha@zenrocklabs.io>
2025-01-12 23:15:07 +0000

    fix: migration version (#19)

--- x/zenbtc/module/module.go
+++ x/zenbtc/module/module.go
@@ -126,7 +126,7 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 
 	m := keeper.NewMigrator(&am.keeper)
-	if err := cfg.RegisterMigration(types.ModuleName, 2, m.Migrate1to2); err != nil {
+	if err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2); err != nil {
 		panic(fmt.Sprintf("failed to migrate x/%s from version 1 to 2: %v", types.ModuleName, err))
 	}
 }
