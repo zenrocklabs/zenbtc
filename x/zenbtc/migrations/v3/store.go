319c7dc07e89629da339cb1cbc6458a8cfd88fad
Valdi Kamenarov <6217014+valdikamenarov@users.noreply.github.com>
2025-01-28 11:31:50 +0200

    feat: Add caip2 id field (#22)
    * Add caip2 id field
    
    * add migration
    
    * add migration tests and handle edge case
    
    * fix migration test
    
    * fix tests
    
    * add error checks and fix test case
    
    * add missing error check

---
+++ x/zenbtc/migrations/v3/store.go
@@ -0,0 +1,39 @@
+package v3
+
+import (
+	"errors"
+	"fmt"
+
+	"cosmossdk.io/collections"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/zenrocklabs/zenbtc/x/zenbtc/types"
+)
+
+func ChangePendingMintTxChainIdtoCaip2Id(ctx sdk.Context, pendingMintTxCol collections.Item[types.PendingMintTransactions], codec codec.BinaryCodec) error {
+	p, err := pendingMintTxCol.Get(ctx)
+	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return nil
+		}
+		return err
+	}
+
+	pTxs := types.PendingMintTransactions{}
+
+	for _, pendingMintTx := range p.Txs {
+		switch pendingMintTx.ChainType {
+		case types.WalletType_WALLET_TYPE_EVM:
+			pendingMintTx.Caip2ChainId = fmt.Sprintf("eip155:%d", pendingMintTx.ChainId)
+		}
+
+		pTxs.Txs = append(pTxs.Txs, pendingMintTx)
+	}
+
+	if err := pendingMintTxCol.Set(ctx, pTxs); err != nil {
+		return err
+	}
+
+	return nil
+}
