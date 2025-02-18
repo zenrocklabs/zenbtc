e44a7e7f0059d3c71cc0f9a8f3a5b5d97e1f485d
Martin <martin@zenrocklabs.io>
2025-02-17 22:45:14 -0700

    chore: squash migrations into v2

--- x/zenbtc/keeper/migrator.go
+++ x/zenbtc/keeper/migrator.go
@@ -3,8 +3,6 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v2 "github.com/zenrocklabs/zenbtc/x/zenbtc/migrations/v2"
-	v3 "github.com/zenrocklabs/zenbtc/x/zenbtc/migrations/v3"
-	v4 "github.com/zenrocklabs/zenbtc/x/zenbtc/migrations/v4"
 	"github.com/zenrocklabs/zenbtc/x/zenbtc/types"
 )
 
@@ -22,17 +20,23 @@ func NewMigrator(keeper *Keeper) Migrator {
 
 // Migrate1to2 migrates x/zenbtc state from consensus version 1 to 2.
 func (m Migrator) Migrate1to2(ctx sdk.Context) error {
-	return v2.UpdateParams(ctx, m.keeper.Params)
-}
 
-func (m Migrator) Migrate2to3(ctx sdk.Context) error {
-	return v3.RemoveBadTestnetState(ctx, m.keeper.PendingMintTransactions, m.keeper.cdc)
-}
+	if err := v2.UpdateParams(ctx, m.keeper.Params); err != nil {
+		return err
+	}
 
-func (m Migrator) Migrate3to4(ctx sdk.Context) error {
+	if err := v2.RemoveBadTestnetState(ctx, m.keeper.PendingMintTransactions, m.keeper.cdc); err != nil {
+		return err
+	}
+
 	shim := func(ctx sdk.Context, tx *types.PendingMintTransaction) error {
 		_, err := m.keeper.CreatePendingMintTransaction(ctx, tx)
 		return err
 	}
-	return v4.ChangePendingMintTxChainIdtoCaip2Id(ctx, m.keeper.PendingMintTransactions, shim)
+
+	if err := v2.ChangePendingMintTxChainIdtoCaip2Id(ctx, m.keeper.PendingMintTransactions, shim); err != nil {
+		return err
+	}
+
+	return nil
 }
