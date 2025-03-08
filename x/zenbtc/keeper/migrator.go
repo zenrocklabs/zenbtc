9a48662250b40edaf5fd5c6c6d8fc225f907f749
Sasha Duke <sasha@zenrocklabs.io>
2025-03-08 04:46:34 +0000

    fix: add corrected params migration

--- x/zenbtc/keeper/migrator.go
+++ x/zenbtc/keeper/migrator.go
@@ -4,6 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v2 "github.com/zenrocklabs/zenbtc/x/zenbtc/migrations/v2"
 	v3 "github.com/zenrocklabs/zenbtc/x/zenbtc/migrations/v3"
+	v4 "github.com/zenrocklabs/zenbtc/x/zenbtc/migrations/v4"
 )
 
 // Migrator is a struct for handling in-place store migrations.
@@ -32,6 +33,16 @@ func (m Migrator) Migrate1to2(ctx sdk.Context) error {
 func (m Migrator) Migrate2to3(ctx sdk.Context) error {
 
 	if err := v3.UpdateParams(ctx, m.keeper.Params); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Migrate3to4 migrates x/zenbtc params from consensus version 3 to 4.
+func (m Migrator) Migrate3to4(ctx sdk.Context) error {
+
+	if err := v4.UpdateParams(ctx, m.keeper.Params); err != nil {
 		return err
 	}
 
