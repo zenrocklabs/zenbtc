d7625449cd98655ee04292efc303014e7529d1af
Sasha Duke <sasha@zenrocklabs.io>
2025-03-26 03:59:05 +0100

    wip

--- x/zenbtc/types/keys.go
+++ x/zenbtc/types/keys.go
@@ -14,33 +14,35 @@ const (
 )
 
 var (
-	ParamsKey                       = collections.NewPrefix(0)
-	LockTransactionsKey             = collections.NewPrefix(1)
-	PendingMintTransactionsKey      = collections.NewPrefix(2)
-	RedemptionsKey                  = collections.NewPrefix(3)
-	SupplyKey                       = collections.NewPrefix(4)
-	PendingMintTransactionCountKey  = collections.NewPrefix(5)
-	BurnEventsKey                   = collections.NewPrefix(6)
-	PendingMintTransactionsMapKey   = collections.NewPrefix(7)
-	BurnEventCountKey               = collections.NewPrefix(8)
-	FirstPendingMintTransactionKey  = collections.NewPrefix(9)
-	FirstPendingBurnEventKey        = collections.NewPrefix(10)
-	FirstPendingRedemptionKey       = collections.NewPrefix(11)
-	FirstPendingStakeTransactionKey = collections.NewPrefix(12)
-	FirstRedemptionAwaitingSignKey  = collections.NewPrefix(13)
+	ParamsKey                         = collections.NewPrefix(0)
+	LockTransactionsKey               = collections.NewPrefix(1)
+	PendingMintTransactionsKey        = collections.NewPrefix(2)
+	RedemptionsKey                    = collections.NewPrefix(3)
+	SupplyKey                         = collections.NewPrefix(4)
+	PendingMintTransactionCountKey    = collections.NewPrefix(5)
+	BurnEventsKey                     = collections.NewPrefix(6)
+	PendingMintTransactionsMapKey     = collections.NewPrefix(7)
+	BurnEventCountKey                 = collections.NewPrefix(8)
+	FirstPendingEthMintTransactionKey = collections.NewPrefix(9)
+	FirstPendingBurnEventKey          = collections.NewPrefix(10)
+	FirstPendingRedemptionKey         = collections.NewPrefix(11)
+	FirstPendingStakeTransactionKey   = collections.NewPrefix(12)
+	FirstRedemptionAwaitingSignKey    = collections.NewPrefix(13)
+	FirstPendingSolMintTransactionKey = collections.NewPrefix(14)
 
-	ParamsIndex                       = "params"
-	LockTransactionsIndex             = "lock_transactions"
-	PendingMintTransactionsIndex      = "pending_mint_transactions"
-	RedemptionsIndex                  = "redemptions"
-	SupplyIndex                       = "supply"
-	PendingMintTransactionCountIndex  = "pending_mint_transaction_count"
-	BurnEventsIndex                   = "burn_events"
-	PendingMintTransactionsMapIndex   = "pending_mint_transactions_map"
-	BurnEventCountIndex               = "burn_event_count"
-	FirstPendingMintTransactionIndex  = "first_pending_mint_transaction"
-	FirstPendingStakeTransactionIndex = "first_pending_stake_transaction"
-	FirstPendingBurnEventIndex        = "first_pending_burn_event"
-	FirstPendingRedemptionIndex       = "first_pending_redemption"
-	FirstRedemptionAwaitingSignIndex  = "first_redemption_awaiting_sign"
+	ParamsIndex                         = "params"
+	LockTransactionsIndex               = "lock_transactions"
+	PendingMintTransactionsIndex        = "pending_mint_transactions"
+	RedemptionsIndex                    = "redemptions"
+	SupplyIndex                         = "supply"
+	PendingMintTransactionCountIndex    = "pending_mint_transaction_count"
+	BurnEventsIndex                     = "burn_events"
+	PendingMintTransactionsMapIndex     = "pending_mint_transactions_map"
+	BurnEventCountIndex                 = "burn_event_count"
+	FirstPendingEthMintTransactionIndex = "first_pending_eth_mint_transaction"
+	FirstPendingSolMintTransactionIndex = "first_pending_sol_mint_transaction"
+	FirstPendingStakeTransactionIndex   = "first_pending_stake_transaction"
+	FirstPendingBurnEventIndex          = "first_pending_burn_event"
+	FirstPendingRedemptionIndex         = "first_pending_redemption"
+	FirstRedemptionAwaitingSignIndex    = "first_redemption_awaiting_sign"
 )
